33c838f8417c804dce8f77ee4170c8a100c2f208
MR_Kotuk <mrkotuk333@gmail.com>
2024-06-18 23:24:39 +0200

    0.7 Switching Measurement With Shader

---
+++ Assets/Dissolve/Examples/scripts/GizmoLine.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoLine : MonoBehaviour 
+{
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(this.transform.position, this.transform.position + Vector3.right);
+    }
+}
--- Assets/Scripts/Measurements.cs
+++ Assets/Scripts/Measurements.cs
@@ -1,5 +1,7 @@
 using Unity.AI.Navigation;
 using UnityEngine;
+using System.Collections;
+using CandyCoded.HapticFeedback;
 
 public class Measurements : MonoBehaviour
 {
@@ -8,22 +10,73 @@ public class Measurements : MonoBehaviour
     [SerializeField] private GameObject _anotherMeasurement;
     [Space]
 
+    [Header("Shader Activate/Disactivate")]
+    [SerializeField] private Material _mesurementMaterial;
+    [SerializeField] private Material _anotherMesurementMaterial;
+
+    [SerializeField] private float _dissolveSpeed;
+    [SerializeField] private float _dissolveDelay;
+
+    [SerializeField] private float _maxAmount;
+    [SerializeField] private float _minAmount;
+    [Space]
+
     [Header("Scripts")]
     [SerializeField] private NavMeshSurface _surfcase;
 
+    private bool isMeasurement = true;
+
     private void Start()
     {
+        MeasurementStartActive();
+    }
+
+    private void MeasurementStartActive()
+    {
         _measurement.SetActive(true);
-        _anotherMeasurement.SetActive(false);
+        _anotherMeasurement.SetActive(true);
+
+        MeasurementSwitchDissolve(true);
     }
 
     public void SwitchMeasurement()
     {
-        Handheld.Vibrate();
-        
-        _measurement.SetActive(!_measurement.activeSelf);
-        _anotherMeasurement.SetActive(!_anotherMeasurement.activeSelf);
+        HapticFeedback.LightFeedback();
+
+        isMeasurement = !isMeasurement;
+
+        MeasurementSwitchDissolve(isMeasurement);
+    }
+
+    private void MeasurementSwitchDissolve(bool isActive)
+    {
+        SwitchActive(isMeasurement, true);
+
+        StartCoroutine(Dissolve(isActive ? _anotherMesurementMaterial : _mesurementMaterial, -_dissolveSpeed, _minAmount));
+        StartCoroutine(Dissolve(isActive ? _mesurementMaterial : _anotherMesurementMaterial, _dissolveSpeed, _maxAmount));
+
+        SwitchActive(!isMeasurement, false);
 
         _surfcase.BuildNavMesh();
+    }
+
+    private void SwitchActive(bool isMeasurement, bool isActive)
+    {
+        if (isMeasurement)
+            _measurement.SetActive(isActive);
+        else
+            _anotherMeasurement.SetActive(isActive);
+    }
+
+    private IEnumerator Dissolve(Material material, float speed, float target)
+    {
+        float dissolveAmount = material.GetFloat("_Height");
+
+        while ((speed > 0 && dissolveAmount < target) || (speed < 0 && dissolveAmount > target))
+        {
+            dissolveAmount += speed;
+            material.SetFloat("_Height", dissolveAmount);
+            yield return new WaitForSeconds(_dissolveDelay);
+        }
     }
 }
